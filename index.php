97c9a91c7a23bf017e88d167b69806ff0e558d5d
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-03-19 19:53:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,67 +26,38 @@
 	</div>
 	
 	    <div class ="content">
+		    
 	<?php
 	if($login_button == ''){
 	 switch ($load) {
-	 case 'list':
-			 require_once('list.php');
-				break;
+	 	case 'list':
+			require_once('list.php');
+			break;
 		case 'detail':
 			 require_once('detail.php');
-				break;
+			break;
 		case 'categories':
 			require_once('categories.php');
-			    break;
-        case 'add':
-        	require_once('add.php');
+			break;
+        	case 'add':
+        		require_once('add.php');
         		break;
-        case 'update':
-            require_once('update.php');
-				break;
+        	case 'update':
+            		require_once('update.php');
+			break;
 		case 'home':
 			require_once('home.php');
-				break;		
+			break;		
 		default:
 			require_once('home.php');
 			break;
 		}
 	}else{
 	  echo '<div align="center">'.$login_button . '</div>';
+	  echo '<div align="center">' .$facebook_login_url. '</div>';
 	}
 	?>
-		<br>
-		<br>
-
-		<?php
-		if(isset($facebook_login_url)){
-            echo '<div align="center">' .$facebook_login_url. '</div>';
-            }else{
-	 switch ($load) {
-	 case 'list':
-			 require_once('list.php');
-				break;
-		case 'detail':
-			 require_once('detail.php');
-				break;
-		case 'categories':
-			require_once('categories.php');
-			    break;
-        case 'add':
-        	require_once('add.php');
-        		break;
-        case 'update':
-            require_once('update.php');
-				break;
-		case 'home':
-			require_once('home.php');
-				break;		
-		default:
-			require_once('home.php');
-			break;
-		}
-			}	
-			?>	
+	
 		</div> 
 	  
 </div>
