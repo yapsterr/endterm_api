59e7a996f50348390790fc8b8c14a3d62c67f466
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-03-20 13:57:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 	    <div class ="content">
 		    
 	<?php
-	if($login_button == ''){
+	if($login_button == '' OR $facebook_login_url == ''){
 	 switch ($load) {
 	 	case 'list':
 			require_once('list.php');
@@ -53,40 +53,10 @@
 			break;
 		}
 	}else{
-	  echo '<div align="center">'.$login_button . '</div>';
+	  echo '<div align="center">'.$login_button . '</div>'; <br>
+	  echo '<div align="center">' .$facebook_login_url. '</div>';
 	}
 	?>
-		    
-		    <br>
-	<?php
-		if(isset($facebook_login_url)){
-            		echo '<div align="center">' .$facebook_login_url. '</div>';
-           		 }else{
-		 switch ($load) {
-	 		case 'list':
-				 require_once('list.php');
-				 break;
-			case 'detail':
-				 require_once('detail.php');
-				 break;
-			case 'categories':
-				require_once('categories.php');
-			    	break;
-        		case 'add':
-        			require_once('add.php');
-        			break;
-        		case 'update':
-            			require_once('update.php');
-				break;
-			case 'home':
-				require_once('home.php');
-				break;		
-			default:
-				require_once('home.php');
-				break;
-		}
-			}	
-	?>	
 		</div> 
 	  
 </div>
